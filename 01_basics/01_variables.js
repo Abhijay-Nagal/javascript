1099ea6337e5da882545066b4053e467e21c097b
Abhijay-Nagal <nagalabhijay@gmail.com>
2026-06-07 19:09:12 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 144553
+let accountEmail = "abhijay@gmail.com"
+var password = "4321"
+city = "Shimla"
+console.table([accountId, accountEmail, password, city])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof 1)
+console.log(typeof "Abhijay")
+console.log(typeof true)
